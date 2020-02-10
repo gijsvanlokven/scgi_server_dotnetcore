27f93034ada748575fa6524bc92b8db703d8c63b
Gijs van Lokven <gijs.v.lokven@omnilan.eu>
2020-02-10 13:34:46 +0100

    Add project files.

---
+++ scgi_server_dotnetcore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace scgi_server_dotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
